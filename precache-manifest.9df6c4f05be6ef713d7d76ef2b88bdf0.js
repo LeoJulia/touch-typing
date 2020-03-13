6bc4efcf0df20a82f391b456c16f66e93b2b70ca
Julia Leonteva <julia_leonteva@epam.com>
2020-03-13 11:48:40 +0300

    Updates

---
+++ precache-manifest.9df6c4f05be6ef713d7d76ef2b88bdf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf8c9c23a707dca852779a1b1823116",
+    "url": "/touch-typing/index.html"
+  },
+  {
+    "revision": "ca233b7689a4ecb24b7c",
+    "url": "/touch-typing/static/css/main.469cc129.chunk.css"
+  },
+  {
+    "revision": "cb26724b47c4053ad977",
+    "url": "/touch-typing/static/js/2.f86bce18.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/touch-typing/static/js/2.f86bce18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca233b7689a4ecb24b7c",
+    "url": "/touch-typing/static/js/main.48582759.chunk.js"
+  },
+  {
+    "revision": "1a603ebd298ae1ee422b",
+    "url": "/touch-typing/static/js/runtime-main.77cc1f0e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/touch-typing/static/media/logo.5d5d9eef.svg"
+  }
+]);
