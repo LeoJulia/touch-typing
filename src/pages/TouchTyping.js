23fe1272d11bad8c17e874f7bef182f69a5e9e75
Julia Leonteva <julia_leonteva@epam.com>
2020-03-28 18:23:37 +0300

    add text field and fix configs

---
+++ src/pages/TouchTyping.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { TextField } from '../components';
+
+import styles from './TouchTyping.scss';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <TextField text="It is first line of my project" />
+    </div>
+  );
+}
+
+export default App;
