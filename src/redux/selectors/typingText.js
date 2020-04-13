46833577e7ea1350aba89b75de0e075cd85266b0
Julia Leonteva <julia_leonteva@epam.com>
2020-04-13 21:17:41 +0300

    [JL] #13: remove redundant code

--- src/redux/selectors/typingText.js
+++ src/redux/selectors/typingText.js
@@ -1,5 +1,4 @@
 export const getActiveText = ({ typingText }) => (typingText ? typingText.activeText : '');
-export const getStartData = ({ typingText }) => (typingText ? typingText.isStart : false);
 export const getFinishData = ({ typingText }) => (typingText ? typingText.isFinish : false);
 export const getTimerData = ({ typingText }) => (typingText ? typingText.isTimer : false);
 export const getStartTime = ({ typingText }) => (typingText ? typingText.startTime : null);
