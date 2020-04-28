4dedc454b37aaa2421043d386ef473d997de3c6a
Julia Leonteva <julia_leonteva@epam.com>
2020-04-28 20:18:19 +0300

    remove unnecessary files

--- src/redux/consts/actions.js
+++
@@ -1,4 +0,0 @@
-export const FINISH = 'FINISH';
-export const START_TIMER = 'START_TIMER';
-export const START_RACE = 'START_RACE';
-export const RESTART_RACE = 'RESTART_RACE';
--- src/redux/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-
-import { typingText } from './typingText';
-
-export const reducer = combineReducers({ typingText });
